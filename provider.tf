8b7af3e34f87734e1dffc2261f524d9606908a09
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-06-25 17:16:38 -0400

    more

--- provider.tf
+++ provider.tf
@@ -20,9 +20,9 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
+  # client_id       = var.azure_client_id
+  # client_secret   = var.azure_client_secret
+  # tenant_id       = var.azure_tenant_id
 }
 
 provider "illumio-cloudsecure" {
