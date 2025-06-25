18cfbcacb54eb1940bb3f3280ca41300637e9dad
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2025-06-25 13:21:41 -0400

    Add Azure plan into the overfall plan

--- main.tf
+++ main.tf
@@ -8,3 +8,18 @@ module "aws_account_dev" {
     Owner = "Engineering"
   }
 }
+
+module "azure_subscription_dev" {
+  source                 = "illumio/cloudsecure/illumio//modules/azure_subscription"
+  version                = "1.5.1"
+  name                   = "Test Azure Subscription"
+  mode                   = "ReadWrite"
+#   secret_expiration_days = 365
+#   subscription_id        = var.azure_subscription_id # Azure Subscription ID
+#   tenant_id              = var.azure_tenant_id # Azure Tenant ID
+
+  tags = [
+    "Environment=Dev",
+    "Owner=John Doe"
+  ]
+}
--- provider.tf
+++ provider.tf
@@ -11,6 +11,20 @@ provider "aws" {
   region = "us-west-1"
 }
 
+provider "azurerm" {
+  features {}
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+}
+
+provider "azuread" {
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
+
 provider "illumio-cloudsecure" {
   client_id     = var.illumio_cloudsecure_client_id
   client_secret = var.illumio_cloudsecure_client_secret
